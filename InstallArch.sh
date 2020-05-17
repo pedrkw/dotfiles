bc9aa5cadc75d2c69a6d380d1822e84eb8d9e7c0
pedrkw <pj38960@gmail.com>
2020-05-17 13:53:37 -0300

    modifications made to the files and inclusion of new files

--- InstallArch.sh
+++ InstallArch.sh
@@ -1,42 +1,37 @@
 #!/bin/bash
 
 clear
-     echo "Eu não me responsabilizo por qualquer uso indevido"
-     echo "deste script, ele foi criado para"
-     echo "uso pessoal e armazenado publicamente no github. by @pedrkw"
-     echo "e armazenado publicamente no github. @pedrkw"
-     echo "SCRIPT FEITO PARA USAR EM MODO BIOS/MBR/LEGACY!!"
-     echo "QUALQUER DUVIDA EXTRA EU INDICO E RECOMENDO QUE USE O"
-     echo "SITE OFICIAL archlinux.org"
+     echo "   Eu não me responsabilizo por qualquer uso indevido            "
+     echo "   deste script, ele foi criado para                             "
+     echo "   uso pessoal e armazenado publicamente no github. by @pedrkw   "
+     echo "   e armazenado publicamente no github. @pedrkw                  "
+     echo "   SCRIPT FEITO PARA USAR EM MODO BIOS/MBR/LEGACY!!              "
+     echo "   QUALQUER DUVIDA EXTRA EU INDICO E RECOMENDO QUE USE O         "
+     echo "   SITE OFICIAL archlinux.org"
 
 sleep 10
 
 reset
 
      echo "Configurando teclado"
-     ls /usr/share/kbd/keymaps/**/*.map.gz
-     echo "Digite o layout do seu teclado"
-     echo "Exemplo: br-abnt2"
-     read teclado
-loadkeys $teclado
+loadkeys br-abnt2
      echo "Teclado configurado"
-sleep 02
+sleep 01
 
 clear
      echo "A seguir vamos definir a linguagem do ambiente live"
      echo "descomente a opção que corresponder a sua linguagem"
 sleep 01
 
-vim /etc/locale.gen
-
+sed -i  '/pt_BR/,+1 s/^#//' /etc/locale.gen
 locale-gen
       echo "Arquivo Gerado com sucesso"
-sleep 02
+sleep 01
 
 clear
       echo "Atualizando rélogio do sistema"
 timedatectl set-ntp true
-sleep 02
+sleep 01
 
 clear
        echo "Iniciando o particionado"
@@ -79,136 +74,18 @@ sleep 02
 clear
         echo "Iniciando a instalação dos pacotes essenciais"
 
-pacstrap /mnt base base-devel linux linux-firmware dhcpcd vim Networkmanager
+pacstrap /mnt base base-devel linux linux-firmware dhcpcd vim
 
          echo "Comando pacstrap executado com sucesso"
-sleep 02
+sleep 01
 
          echo "Gerando fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
          echo "Fstab gerado com sucesso"
 cat /mnt/etc/fstab
-
-sleep 03
-
-         echo "Entrando em chroot"
-         echo "Definindo o fuso horario"
-ls /usr/share/zoneinfo/
-         echo "Digite sua região/cidade"
-         echo "Exemplo: America/Sao_Paulo"
-ln -sf /usr/share/zoneinfo/$fuso /etc/localtime
-         echo "Fuso definido com sucesso"
-sleep 02
+sleep 01
 
 clear
-         echo "Sincronizando rélogio do hardware"
-hwclock --systohc
-         echo "Rélogio sincronizado"
-
+         echo "Primeira parte realizada com sucesso, agora"
+         echo "entre em arch-chroot /mnt e rode o segundo script"
 clear
-         echo "Definindo o idioma do sistema"
-         echo "Defina o idioma do sistema"
-         echo "Descomente a seguir o que corresponde"
-         echo "a seu idioma"
-         vim /etc/locale.gen
-
-         echo "Gerando o locale"
-
-         locale-gen
-
-         cat /etc/locale.gen
-         echo "Digite a seguir o que corresponde ao seu idioma"
-         echo "Exemplo: pt_BR.UTF-8 (Para evitar erros tenha certeza de"
-         echo "digitar a seu idioma corretamente)"
-         read locale
-         echo "$locale" > /etc/locale.conf
-         echo "Concluido com sucesso"
-
-clear
-         echo "Defina o hosname (Nome da maquina/sistema)"
-         read maquina
-         echo "$maquina" > /etc/hostname
-
-clear
-         echo "Defina o layout do seu sistema"
-         echo "Exemplo: br-abnt2"
-         read keymap
-         echo "KEYMAP=$keymap" > /etc/vconsole.conf
-
-clear
-         echo "Definindo a configuração de rede"
-         echo "127.0.0.1 localhost.localdomain localhost" > /etc/hosts
-         echo "::1 localhost.localdomain localhost" >> /etc/hosts
-         echo "127.0.1.1 $maquina.localdomain" >> /etc/hosts
-
-         echo "Você utiliza o sistema de IP permanente ?"
-         echo "Seleciona a opção que lhe corresponder melhor"
-         echo "1 - Sim"
-         echo "2 - Não"
-         read opcao
-         if [ $opcao == "1" ];
-         then
-         echo "Então vamos voltar ao /etc/hosts"
-         echo "Lá você vai editar os IP e substituir"
-         echo "Pelo de seu uso, tome cuidado com as alterações"
-         echo "Escolha entre o vim ou nano para editar o arquivo"
-         echo "Se não souber qual escolher use o nano"
-         read editor
-         $editor /etc/hosts
-         echo "Alterado com sucesso"
-         elif [ $opcao == "2" ];
-         then
-         echo "Continuando o script..."
-         sleep 01
-         clear
-         fi
-
-         echo ""
-         echo "nameserver 1.1.1.1" >> /etc/resolv.conf
-         echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-         echo "Concluido"
-         sleep 01
-
-clear
-         echo "A seguir vamos configurar para que o dhcpcd ou Networkmanager"
-         echo "inicie junto do seu sistema quando a instalação finalizar"
-         echo "Se você utiliza internet cabeada digite a seguir dhcpcd"
-         echo "Caso use wifi digite a seguir NetworkManager (Por favor"
-         echo "respeite as letras maisculas ou minusculas"
-         read net
-         systemctl enable $net
-
-clear
-         echo "Agora vamos pra a criação do seu usuario"
-         echo "A seguir digite o nome do seu usuario"
-         read nome
-         useradd -m -g users -G log,sys,wheel,rfkill,dbus -s /bin/bash $nome
-clear
-         echo "A seguir digite a senha do seu usuario"
-         echo "Ela não vai estar visivel mas vai estar"
-         echo "sendo digitada, então tenha cuidado e calma"
-         echo "no momento de digitar"
-         echo "Digite a sua senha:"
-         passwd $nome
-         echo "Senha do $nome difinido/a com sucesso"
-clear
-         echo "Agora defina a senha do usuario ROOT"
-         echo "Essa senha é bastante importante, portanto tome"
-         echo "cuidado para não haver erros de digitação"
-         passwd
-clear
-         echo "Descomentando a linha wheel em /etc/sudoers para"
-         echo "permitir que os usuarios no grupo wheel tenha acesso"
-         echo "ao poder de ROOT usando sudo"
-         sed -i  '/wheel/ s/^#//' /etc/sudoers
-         echo "Descomentado com sucesso"
-clear
-         echo "Instalando BOOTLOADER (GRUB)"
-         mkinitcpio -P
-         grub-install /dev/sda
-         grub-mkconfig -o /boot/grub/grub.cfg
-         echo "BOOTLOADER (GRUB) Instalado com sucesso"
-         echo "Instalação concluida"
-         exit
-         umount -R /mnt
-         reboot
--- InstallArch2.sh
+++ InstallArch2.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# find the current user.
-whoami=$(whoami)
- 
      echo -e "Definindo o fuso horario"
 ln -sf /usr/share/zoneinfo/America/Fortaleza /etc/localtime
 hwclock --systohc
@@ -11,48 +8,53 @@ hwclock --systohc
 sed -i  '/pt_BR/,+1 s/^#//' /etc/locale.gen
 
 locale-gen
-
+     echo "Definindo o idioma do sistema"
      echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
-
+     echo "Idioma definido"
+clear
+     echo "Definindo o nome da maquina/hostname"
      echo "archlinux" > /etc/hostname
-
+     echo "Definido com sucesso"
+clear
+     echo "Definindo layout do teclado"
      echo "KEYMAP=br-abnt2" > /etc/vconsole.conf
-     
-     echo "127.0.0.1 localhost.localdomain localhost" > /etc/hosts
+     echo "Layout definido com sucesso"
+clear
+     echo "Configuranto o arquivo em /etc/hosts"
+     echo "127.0.0.1 localhost.localdomain localhost" >> /etc/hosts
 
      echo "::1 localhost.localdomain localhost" >> /etc/hosts
 
      echo "127.0.1.1 archlinux.localdomain" >> /etc/hosts
-
+     echo "Configurado com sucesso"
+clear
+     echo "Definindo os DNS em /etc/resolv.conf"
      echo "nameserver 1.1.1.1" >> /etc/resolv.conf
 
      echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-
-     echo -e "Concluido"
-
-sleep 05
-
+     echo "Definido com sucesso"
+clear
+     echo "Habilitando o serviço DHCPCD"
 systemctl enable dhcpcd
-
+clear
+     echo "Criando usuario"
 useradd -m -g users -G log,sys,wheel,rfkill,dbus -s /bin/bash pedrkw
-
+clear
+     echo "Digite a senha do usuario"
 passwd pedrkw
-
+clear
+     echo "Digite a senha do usuario ROOT"
 passwd
-
-vim /etc/sudoers
-
-sleep 05
-
-     echo -e "Instalando BOOTLOADER (GRUB)"
+     echo "Descomentando a linha %wheel ALL=(ALL) em /etc/sudoers"
+sed -i '/%wheel ALL=(ALL) ALL/s/^#//' /etc/sudoers
+     echo "Descomentado com sucesso"
+clear
+     echo "Instalando BOOTLOADER (GRUB)"
 mkinitcpio -P
 
 grub-install /dev/sda
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
-sleep 05
-
-      echo -e "Instalação concluida"
-
-sleep 01
+clear
+      echo "Instalação concluida"
