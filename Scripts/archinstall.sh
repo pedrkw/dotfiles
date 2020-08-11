1c34bc22e7487e195b0ce7c5c3b0a23b6e159784
pedrkw <pj38960@gmail.com>
2020-08-11 13:23:51 -0300

    [update] removed the swap partition support and added swapfile support. improvements and minor changes

--- Scripts/archinstall.sh
+++ Scripts/archinstall.sh
@@ -1,39 +1,36 @@
 #!/bin/bash
 clear
-
 RED='\033[1;31m'
 NC='\033[0m'
 
-echo -e "${RED}Script feito por @pedrkw(Telegram)          ${NC}"
-echo -e "${RED}não me resposabilizo por eventuais problemas${NC}"
-echo -e "${RED}Iniciando script...                         ${NC}"
-sleep 05
-
-loadkeys br-abnt2
+echo -e "${RED}Script feito por @pedrkw(Telegram) ${NC}
+${RED}não me resposabilizo por eventuais problemas${NC}
+${RED}Iniciando script...                         ${NC}"
+sleep 02
 sed -i  '/pt_BR.UTF-8/ s/^#//' /etc/locale.gen
 locale-gen
 export LANG=pt_BR.UTF-8
 timedatectl set-ntp true
 clear
-echo -e "${RED}Parte inicial executada com sucesso${NC}"
-echo -e "${RED}Continuando o script...${NC}"
-echo -e "Pressione a tecla ${RED}ENTER${NC} para continuar..."
+echo -e "${RED}Parte inicial executada com sucesso${NC}
+${RED}Continuando o script...${NC}
+Pressione a tecla ${RED}ENTER${NC} para continuar..."
 read $tmp
 clear
 echo -e "Sincronizando espelhos usando o reflector, aguarde..."
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 clear
-echo -e "${RED}A T E N Ç Ã O :${NC}"
-echo -e "Esse script segue um certo padrão de particionamento\nao utilizar esse script você terá que ter uma partição /\numa partição /home\ne uma partição swap\n${RED}no futuro, serão adicionas novas possibilidades${NC}"
-echo -e "Ele lhe dá a possibilidade de utilizar EXT4 ou BTRFS como sistema de arquivos"
-echo -e "Pressione a tecla ${RED}ENTER${NC} para continuar..."
+echo -e "${RED}A T E N Ç Ã O :${NC}
+Esse script segue um certo padrão de particionamento\nao utilizar esse script você terá que ter uma partição /\numa partição /home\ne será criado um swapfile\n${RED}não se faz necessario ciar uma partição swap${NC}
+Ele lhe dá a possibilidade de utilizar EXT4 ou BTRFS como sistema de arquivos
+Pressione a tecla ${RED}ENTER${NC} para continuar..."
 read $tmp
 clear
-echo -e "Deseja iniciar o particionador ?"
-echo -e "Selecione uma opção (digite apenas o número que corresponde a opção desejada):"
-echo "1 - Sim"
-echo "2 - Não"
+echo -e "Deseja iniciar o particionador ?
+Selecione uma opção (digite apenas o número que corresponde a opção desejada):
+1 - Sim
+2 - Não"
 read opcao;
 if [ $opcao == "1" ];
  then
@@ -44,22 +41,17 @@ elif [ $opcao == "2" ];
 fi
 clear
  lsblk
- echo -e "Informe o caminho da partição ${RED}SWAP${NC}"
- echo -e "Informe qual a partição. Ex: (/dev/sda1)"
- read swap
-clear
- lsblk
- echo -e "Informe o caminho da partição ${RED}ROOT${NC}"
- echo -e "Informe qual a partição. Ex: (/dev/sda2)"
+ echo -e "Informe o caminho da partição ${RED}ROOT${NC}
+ Informe qual a partição. Ex: (/dev/sda2)"
  read root
 clear
  lsblk
- echo -e "Informe o caminho da partição ${RED}HOME${NC}"
- echo -e "Informe qual a partição. Ex: (/dev/sda3)"
+ echo -e "Informe o caminho da partição ${RED}HOME${NC}
+ Informe qual a partição. Ex: (/dev/sda3)"
  read home
 clear
-echo -e "Informe o sistema de arquivos que deseja utilizar"
-echo -e "Selecione uma opção (digite apenas o número que corresponde a opção desejada):"
+echo -e "Informe o sistema de arquivos que deseja utilizar
+Selecione uma opção (digite apenas o número que corresponde a opção desejada):"
 echo "1 - BTRFS"
 echo "2 - EXT4"
 read opcao;
@@ -67,43 +59,59 @@ if [ $opcao == "1" ];
  then
  mkfs.btrfs -L root $root
  mkfs.btrfs -L home $home
- mkswap $swap
- swapon $swap
  mount $root /mnt
  mkdir /mnt/home
  mount $home /mnt/home
- echo -e "Iniciando a instalação dos pacotes essenciais"
- echo -e "Por padrão esses serão os pacotes a serem instalados:\n${RED}base base-devel linux linux-firmware dhcpcd vim grub btrfs-progs${NC}"
- echo -e "Caso queira adicionar outros pacotes faça agora\nSe não quiser, apenas aperte ${RED}ENTER${NC} para prosseguir"
- echo -e "Digite os nomes ${RED}CORRETOS${NC} dos pacotes extras que você quer, lembre-se\nde respeitar os espaços entre os nomes dos pacotes" 
+ echo -e "Iniciando a instalação dos pacotes essenciais
+ Por padrão esses serão os pacotes a serem instalados:\n${RED}base base-devel linux linux-firmware dhcpcd vim grub btrfs-progs${NC}
+ Caso queira adicionar outros pacotes faça agora\nSe não quiser, apenas aperte ${RED}ENTER${NC} para prosseguir
+ Digite os nomes ${RED}CORRETOS${NC} dos pacotes extras que você quer, lembre-se\nde respeitar os espaços entre os nomes dos pacotes" 
  read pacotes
  pacstrap /mnt base base-devel linux linux-firmware dhcpcd vim grub btrfs-progs $pacotes
+ echo -e "Gerando fstab..."
+ genfstab -U /mnt >> /mnt/etc/fstab
+ echo -e "Criando swapfile"
+ arch-chroot /mnt /bin/bash -c "truncate -s 0 /swapfile"
+ arch-chroot /mnt /bin/bash -c "chattr +C /swapfile"
+ arch-chroot /mnt /bin/bash -c "btrfs property set /swapfile compression none"
+ echo -e "Digite o tamanho do swapfile em mebibytes ou gibibytes. Ex:1024M ou 1G"
+ read swapfi
+ arch-chroot /mnt /bin/bash -c "fallocate -l $swapfi /swapfile"
+ arch-chroot /mnt /bin/bash -c "chmod 600 /swapfile"
+ arch-chroot /mnt /bin/bash -c "mkswap /swapfile"
+ arch-chroot /mnt /bin/bash -c "swapon /swapfile"
+ arch-chroot /mnt /bin/bash -c "echo /swapfile none swap defaults 0 0 >> /etc/fstab"
 elif [ $opcao == "2" ];
  then
  mkfs.ext4 $root
  mkfs.ext4 $home
- mkswap $swap
- swapon $swap 
  mount $root /mnt
  mkdir /mnt/home
  mount $home /mnt/home
- echo -e "Iniciando a instalação dos pacotes essenciais"
- echo -e "Por padrão esses serão os pacotes a serem instalados:\n${RED}base base-devel linux linux-firmware dhcpcd vim grub${NC}"
- echo -e "Caso queira adicionar outros pacotes faça agora/nSe não quiser, apenas aperte ${RED}ENTER${NC} para prosseguir"
- echo -e "Digite os nomes ${RED}CORRETOS${NC} dos pacotes extras que você quer, lembre-se\n de respeitar os espaços entre os nomes dos pacotes" 
+ echo -e "Iniciando a instalação dos pacotes essenciais
+ Por padrão esses serão os pacotes a serem instalados:\n${RED}base base-devel linux linux-firmware dhcpcd vim grub${NC}
+ Caso queira adicionar outros pacotes faça agora/nSe não quiser, apenas aperte ${RED}ENTER${NC} para prosseguir
+ Digite os nomes ${RED}CORRETOS${NC} dos pacotes extras que você quer, lembre-se\n de respeitar os espaços entre os nomes dos pacotes" 
  read pacotes
  pacstrap /mnt base base-devel linux linux-firmware dhcpcd vim grub $pacotes
+ echo -e "Gerando fstab..."
+ genfstab -U /mnt >> /mnt/etc/fstab
+ echo -e "Criando swapfile
+ Digite o tamanho do swapfile em mebibytes ou gibibytes (Por favor, digite conforme os exemplos, \napenas altera o tamanho como quiser). Ex:1024M ou 1G"
+ read swapfi
+ arch-chroot /mnt /bin/bash -c "fallocate -l $swapfi /swapfile"
+ arch-chroot /mnt /bin/bash -c "chmod 600 /swapfile"
+ arch-chroot /mnt /bin/bash -c "mkswap /swapfile"
+ arch-chroot /mnt /bin/bash -c "swapon /swapfile"
+ arch-chroot /mnt /bin/bash -c "echo /swapfile none swap defaults 0 0 >> /etc/fstab"
 fi
 clear
 echo -e "${RED}Comando pacstrap executado com sucesso${NC}"
-echo -e "Gerando fstab..."
-genfstab -U /mnt >> /mnt/etc/fstab
-cat /mnt/etc/fstab
-sleep 04
+sleep 01
 clear
-echo -e "${RED}Entrando em arch-chroot${NC}"
-echo -e "Escolha a região mais adequada para definir o fuso horário"
-echo -e "Tenha cuidado com maiúsculas e minúsculas"
+echo -e "${RED}Entrando em arch-chroot${NC}
+Escolha a região mais adequada para definir o fuso horário
+Tenha cuidado com maiúsculas e minúsculas"
 ls /usr/share/zoneinfo/America/
 read zone
 arch-chroot /mnt /bin/bash -c "ln -sf /usr/share/zoneinfo/America/$zone /etc/localtime"
@@ -125,17 +133,17 @@ clear
 echo -e "Digite o ${RED}nome do seu usuário${NC}"
 read user
 arch-chroot /mnt /bin/bash -c "useradd -m -g users -G wheel -s /bin/bash $user"
-echo -e "Digite a senha do ${RED}SEU USUÁRIO${NC} a seguir"
-echo -e "Para prosseguir e digitar a senha, pressione a tecla ${RED}ENTER${NC}"
+echo -e "Digite a senha do ${RED}SEU USUÁRIO${NC} a seguir
+Para prosseguir e digitar a senha, pressione a tecla ${RED}ENTER${NC}"
 read $tmp
 arch-chroot /mnt /bin/bash -c "passwd $user"
-echo -e "Digite a senha do ${RED}USUÁRIO ROOT${NC} a seguir"
-echo -e "Para prosseguir e digitar a senha, pressione a tecla ${RED}ENTER${NC}"
+echo -e "Digite a senha do ${RED}USUÁRIO ROOT${NC} a seguir
+Para prosseguir e digitar a senha, pressione a tecla ${RED}ENTER${NC}"
 read $tmp
 arch-chroot /mnt /bin/bash -c "passwd root"
 arch-chroot /mnt /bin/bash -c "sed -i '/%wheel ALL=(ALL) ALL/s/^#//' /etc/sudoers"
 arch-chroot /mnt /bin/bash -c "mkinitcpio -P"
 arch-chroot /mnt /bin/bash -c "grub-install /dev/sda"
 arch-chroot /mnt /bin/bash -c "grub-mkconfig -o /boot/grub/grub.cfg"
-echo -e "${RED}A instalação foi finalizada${NC}"
-echo -e "${RED}Encerrando script, lembre-se\n de sair do arch-chroot e desmontar as partições (exit e umount -R /mnt)${NC}"
+echo -e "${RED}A instalação foi finalizada${NC}
+${RED}Encerrando script, lembre-se\n de sair do arch-chroot e desmontar as partições (exit e umount -R /mnt)${NC}"
