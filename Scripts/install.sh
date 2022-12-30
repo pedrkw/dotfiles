97894c0c0c09b2aff024ad8b59870fbaf42631e6
Pedro Jorge <45081159+pedrkw@users.noreply.github.com>
2022-12-30 20:41:37 -0300

    Update install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -5,9 +5,10 @@ GREEN='\033[1;32m'
 NC='\033[0m'
 #cp /etc/pacman.d/mirrorlist /etc/pacmand./mirrorlist.backup
 reflector --verbose --latest 12 --sort rate --save /etc/pacman.d/mirrorlist
-echo -e "${RED}Hey, don't forget to format disk on${NC} ${GREEN}DOS${NC} ${RED}and create${NC} ${GREEN}ONLY ONE${NC} ${RED}partition..${NC}"
+echo -e "${RED}Hey, don't forget to create${NC} ${GREEN}UEFI${NC} ${RED}partition${NC}"
  read $tmp
   cfdisk -z /dev/sda
+  cfdisk -z /dev/sdb
 echo
   lsblk -f
 echo
@@ -15,14 +16,15 @@ echo -e "Press ${GREEN}enter${NC} to continue"
  read $tmp
   clear
   lvmdiskscan
-  pvcreate /dev/sda1
+  pvcreate /dev/sda2
+  pvcreate /dev/sdb1
   pvscan
 echo -e "Hey, is that ${RED}right${NC} ?"
 echo -e "Press ${GREEN}yes${NC} or ${GREEN}no${NC}"
  read option;
  if [ $option == "yes" ];
  then
-  vgcreate weeb /dev/sda1
+  vgcreate weeb /dev/sda2 /dev/sdb1
  elif [ $option == "no" ];
  then
   exit
@@ -30,22 +32,31 @@ echo -e "Press ${GREEN}yes${NC} or ${GREEN}no${NC}"
   vgs
 echo -e "Press ${GREEN}enter${NC} to continue"
  read $tmp
-  lvcreate -L 20G weeb -n wroot /dev/sda1
-  lvcreate -l 100%FREE weeb -n wswap /dev/sda1
+  lvcreate -l 100%FREE weeb -n wroot /dev/sda2
+  lvcreate -L 16G weeb -n wswap /dev/sdb1
+  lvcreate -L 32G weeb -n wvar /dev/sdb1
+  lvcreate -L 300G weeb -n whdd /dev/sdb1
 echo -e "Press ${GREEN}enter${NC} to continue"
  read $tmp
   clear
   modprobe dm_mod
   vgscan
   vgchange -ay
+  mkfs.fat -F32 /dev/sda1
   mkfs.ext4 /dev/weeb/wroot
+  mkfs.ext4 /dev/weeb/wvar
+  mkfs.ext4 /dev/weeb/whdd
   mkswap /dev/weeb/wswap
 echo -e "-"
   lsblk -f
 echo -e "Press ${GREEN}enter${NC} to continue"
  read $tmp
-  swapon /dev/weeb/wswap
-  mount /dev/weeb/wroot /mnt
+mount /dev/weeb/wroot /mnt
+mkdir -p /mnt/{var,hdd,boot/efi}
+mount /dev/sda1 /mnt/boot/efi
+mount /dev/weeb/wvar /mnt/var
+mount /dev/weeb/whdd /mnt/hdd
+swapon /dev/weeb/wswap
  sleep 05
   clear
   lsblk -f
@@ -57,7 +68,7 @@ echo -e "Remember: ${GREEN}downlods${NC} and ${GREEN}multilib${NC}"
 echo -e "Press ${GREEN}enter${NC} to continue"
  read $tmp
  vim /etc/pacman.conf
-  pacstrap -K /mnt linux linux-firmware base base-devel networkmanager vim grub efibootmgr xorg-server xorg-xinit fish lvm2 firefox gnu-free-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts ttf-liberation ttf-croscore ttf-dejavu ttf-bitstream-vera ttf-droid ttf-croscore xfce4 xfce4-goodies lightdm-gtk-greeter lightdm-gtk-greeter-settings lightdm  ttf-ibm-plex git
+  pacstrap -K /mnt linux linux-firmware base base-devel latte-dock plasma-meta konsole sddm dolphin flameshot bash-completion easyeffects pipewire lib32-pipewire pipewire-alsa pipewire-pulse wireplumber maim persepolis aria2 bitwarden vulkan-radeon xf86-video-amdgpu lib32-vulkan-radeon lib32-mesa libva-mesa-driver lib32-libva-mesa-driver mesa-vdpau lib32-mesa-vdpau steam gamemode lib32-gamemode libva-vdpau-driver libvdpau-va-gl vulkan-tools obsidian obs-studio papirus-icon-theme materia-gtk-theme discord mpv giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs lutris networkmanager vim grub xorg-server xorg-xinit fish lvm2 firefox gnu-free-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts ttf-liberation ttf-croscore ttf-dejavu ttf-bitstream-vera ttf-droid ttf-croscore ttf-ibm-plex git xdg-user-dirs openssh ufw gufw gparted pacman-contrib authy flameshot
   genfstab -U /mnt > /mnt/etc/fstab
   cat /mnt/etc/fstab
 echo -e " All right ?"
@@ -74,7 +85,7 @@ arch-chroot /mnt /bin/bash -c "pacman -U --noconfirm 'https://cdn-mirror.chaotic
 arch-chroot /mnt /bin/bash -c "echo "[chaotic-aur]" >> /etc/pacman.conf"
 arch-chroot /mnt /bin/bash -c "echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf"
 arch-chroot /mnt /bin/bash -c "vim /etc/pacman.conf"
-arch-chroot /mnt /bin/bash -c "pacman -Syu paru linux-tkg-cfs-generic_v3 ttf-juliamono spotify anydesk-bin visual-studio-code-bin droidcam mangohud lib32-mangohud heroic-games-launcher-bin wine-tkg-staging-fsync-git vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm"
+arch-chroot /mnt /bin/bash -c "pacman -Syu paru carla linux-tkg-cfs-generic_v3 menulibre ttf-juliamono spotify anydesk-bin visual-studio-code-bin droidcam mangohud lib32-mangohud heroic-games-launcher-bin wine-tkg-staging-fsync-git vulkan-icd-loader lib32-vulkan-icd-loader noto-color-emoji-fontconfig --noconfirm"
 arch-chroot /mnt /bin/bash -c "ln -sf /usr/share/zoneinfo/America/Fortaleza /etc/localtime"
 arch-chroot /mnt /bin/bash -c "hwclock --systohc"
 arch-chroot /mnt /bin/bash -c "sed -i '/en_US.UTF-8/s/^# //' /etc/locale.gen"
@@ -83,7 +94,7 @@ arch-chroot /mnt /bin/bash -c "echo LANG=en_US.UTF-8 > /etc/locale.conf"
 arch-chroot /mnt /bin/bash -c "echo deltarch >> /etc/hostname"
 arch-chroot /mnt /bin/bash -c "echo KEYMAP=br-abnt2 >> /etc/vconsole.conf"
 arch-chroot /mnt /bin/bash -c "systemctl enable NetworkManager"
-arch-chroot /mnt /bin/bash -c "systemctl enable lightdm"
+arch-chroot /mnt /bin/bash -c "systemctl enable sddm"
 arch-chroot /mnt /bin/bash -c "useradd -m -g users -s /bin/fish pedrokw"
 echo -e "${RED}Chose your password${NC} ${GREEN}(user account)"
 echo -e "Press ${GREEN}enter${NC} to continue"
@@ -93,8 +104,7 @@ echo -e "Press ${GREEN}enter${NC} to continue"
 arch-chroot /mnt /bin/bash -c "passwd root"
 echo -e "Press ${GREEN}enter${NC} to continue and edit ${RED}sudoers file${NC}"
 arch-chroot /mnt /bin/bash -c "visudo /etc/sudoers"
-#arch-chroot /mnt /bin/bash -c "grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinuxGRUB"
-arch-chroot /mnt /bin/bash -c "grub-install /dev/sda"
+arch-chroot /mnt /bin/bash -c "grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinuxGRUB"
 echo -e "Heey, don't forget, ${RED}lvm${NC} module (/etc/default/grub) is important"
 echo -e "Press ${GREEN}enter${NC} to continue"
  read $tmp
