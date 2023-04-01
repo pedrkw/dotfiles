b3ca0df736d23e46ca6d3bbb80f60d801b548eb5
Pedro Jorge <45081159+pedrkw@users.noreply.github.com>
2023-04-01 12:48:15 -0300

    rewrite script

--- scripts/gravardd.sh
+++ scripts/gravardd.sh
@@ -1,21 +1,50 @@
 #!/bin/bash
-# find the current user.
+#find the current user
 whoami=$(whoami)
- 
-            echo -e "Insira o caminha da iso para gravar"
-            echo -e "Por exemplo: ~/Downloads/archlinux.iso"
-            read iso
-            
-            echo -e "Se não sabe qual a partição do drive a ser usado"
-            echo -e "escolha qual vai ser o drive a ser usado" 
-lsblk      
-            echo -e "Informe qual o drive para ser usado"
-            echo -e "exemplo: /dev/sdx (troque o x pela letra correspondente "
-            read devsda
-            
-            echo -e "A iniciar a gravação, aguarde."
+
+#ask for iso file path
+echo -e "Insira o caminho da ISO para gravar"
+echo -e "Por exemplo: ~/Downloads/archlinux.iso"
+read iso
+
+#verify iso path
+if [ ! -f $iso ]; then
+echo "O caminho do arquivo ISO é inválido. Por favor, verifique e tente novamente."
+exit 1
+fi
+
+#list available devices and ask for device to be used
+echo -e "Se não sabe qual a partição do drive a ser usado"
+echo -e "escolha qual vai ser o drive a ser usado"
+lsblk
+echo -e "Informe qual o drive para ser usado"
+echo -e "exemplo: /dev/sdx (troque o x pela letra correspondente) "
+read devsda
+
+#verify device path
+if [ ! -b $devsda ]; then
+echo "O caminho do dispositivo é inválido. Por favor, verifique e tente novamente."
+exit 1
+fi
+
+#ask for confirmation before starting the copy process
+echo -e "Você tem certeza de que deseja iniciar a gravação do ISO em $devsda?"
+read -p "Digite 'sim' para confirmar: " confirmation
+
+if [ "$confirmation" != "sim" ]; then
+echo "A gravação do ISO foi cancelada."
+exit 1
+fi
+
+#verify write permission
+if [ ! -w $devsda ]; then
+echo "Você não tem permissão para gravar neste dispositivo. Por favor, execute o script como root ou como um usuário com permissão de gravação no dispositivo."
+exit 1
+fi
+
+#start copy process
+echo -e "Iniciando gravação, aguarde..."
 dd if=$iso of=$devsda bs=4M oflag=sync status=progress
 
-            echo -e "Gravação terminada."
-
-sleep 02
+#print message when copy process is completed
+echo -e "Gravação concluída."
